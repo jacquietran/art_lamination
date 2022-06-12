39cb94e5174f5505d6554e7bb75913ea7606a2fc
Jacquie Tran <jacquie.tran@gmail.com>
2022-06-12 20:06:51 +1200

    more pieces

---
+++ R/lamination_0009.R
@@ -0,0 +1,87 @@
+# Source custom functions ------------------------------------------------------
+
+source(here::here("R/functions/density_grid.R"))
+source(here::here("R/functions/density_plot.R"))
+source(here::here("R/functions/gridded_plots.R"))
+
+# Modifiable parameters --------------------------------------------------------
+
+# Modifiable params
+iteration_id <- "lamination_0009"
+initial_seed <- 5787009
+grid_width <- 100
+grid_height <- 1
+plot_bg <- "#131313"
+plot_res <- 100
+
+# Gradient colours
+base_colours <- c("#00BFB2", "#028090", "#F0F3BD", "#CA5369", "#BC386B")
+
+gradient_colours1 <- c(base_colours, "#71A9F7", "#336699")
+set.seed(initial_seed)
+gradient_source1 <- sample(
+  gradient_colours1, size = length(gradient_colours1), replace = FALSE)
+
+gradient_colours2 <- c(base_colours, "#FF8811", "#DE603E")
+set.seed(initial_seed)
+gradient_source2 <- sample(
+  gradient_colours2, size = length(gradient_colours2), replace = FALSE)
+
+gradient_colours3 <- c(base_colours, "#F7D6E0", "#F2B5D4")
+set.seed(initial_seed)
+gradient_source3 <- sample(
+  gradient_colours3, size = length(gradient_colours3), replace = FALSE)
+
+# Derived parameters -----------------------------------------------------------
+
+# Set custom colour gradient
+gradient_colours1 <- (grDevices::colorRampPalette(gradient_source1))(10)
+gradient_colours2 <- (grDevices::colorRampPalette(gradient_source2))(10)
+gradient_colours3 <- (grDevices::colorRampPalette(gradient_source3))(10)
+
+palette_list <- tibble::tibble(
+  palette_num = 1:3,
+  colours = list(gradient_colours1, gradient_colours2, gradient_colours3))
+
+# Seed vector for multiple dust runs
+set.seed(initial_seed)
+seed_vec <- sample(seq(1, 1000000, by = 1), 2, replace = FALSE)
+
+# Create data ------------------------------------------------------------------
+
+data <- density_grid(
+  seed = initial_seed, n_cols = grid_width, n_rows = grid_height)
+
+# Build plots ------------------------------------------------------------------
+
+# Colourful 2D density plots
+plots_in_grid <- gridded_plots(
+  seed = initial_seed, data = data, palette_list = palette_list,
+  n_cols = grid_width, n_rows = grid_height, resolution = plot_res,
+  bg_colour = plot_bg, visible_grid = FALSE)
+
+# Export to file ---------------------------------------------------------------
+
+# Colourful 2D density plots
+ggplot2::ggsave(
+  here::here(glue::glue("img/ingredients/{`iteration_id`}_base.png")),
+  plots_in_grid, width = 10000, height = 100, units = "px",
+  dpi = 600, device = ragg::agg_png)
+
+# Shift to {magick} workflow ---------------------------------------------------
+
+# Read in images
+img <- magick::image_read(
+  here::here(glue::glue("img/ingredients/{`iteration_id`}_base.png")))
+
+img_rescaled <- img |>
+  magick::image_scale("2000x2000!") |>
+  magick::image_scale("4000x4000!") |>
+  magick::image_noise(noisetype = "gaussian") |>
+  magick::image_noise(noisetype = "gaussian")
+
+# Export
+magick::image_write(
+  img_rescaled,
+  path = here::here(glue::glue("img/{`iteration_id`}.png")),
+  format = "png")
